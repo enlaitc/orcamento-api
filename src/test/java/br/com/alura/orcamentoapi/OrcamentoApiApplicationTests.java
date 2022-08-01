f3caf62fa5f0e647f6b55a1d5ecd640630b09b3a
enlaitc <thiago021299@hotmail.com>
2022-08-01 16:34:10 -0300

    first commit

---
+++ src/test/java/br/com/alura/orcamentoapi/OrcamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.orcamentoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

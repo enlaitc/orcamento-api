ee8d4498a461371093ab55cb567c5135438e0044
enlaitc <thiago021299@hotmail.com>
2022-08-29 15:50:18 -0300

    Autenticação via token JWT para o uso da api

---
+++ src/main/java/br/com/alura/orcamentoapi/security/SecurityConfigurations.java
@@ -0,0 +1,55 @@
+package br.com.alura.orcamentoapi.security;
+
+import br.com.alura.orcamentoapi.repository.UsuarioRepository;
+import br.com.alura.orcamentoapi.service.AutenticacaoService;
+import br.com.alura.orcamentoapi.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaoService authService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(authService).passwordEncoder(new BCryptPasswordEncoder());
+
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/auth").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService,usuarioRepository), UsernamePasswordAuthenticationFilter.class);
+    }
+
+}

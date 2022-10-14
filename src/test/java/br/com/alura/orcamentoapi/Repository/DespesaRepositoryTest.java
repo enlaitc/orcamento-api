5874b704d55672cbcff4f270a839e21469084bd1
enlaitc <thiago021299@hotmail.com>
2022-10-14 11:16:40 -0300

    refactor de despesaRepositoryTest

--- src/test/java/br/com/alura/orcamentoapi/Repository/DespesaRepositoryTest.java
+++ src/test/java/br/com/alura/orcamentoapi/Repository/DespesaRepositoryTest.java
@@ -2,11 +2,9 @@ package br.com.alura.orcamentoapi.Repository;
 
 import br.com.alura.orcamentoapi.model.CategoriaDespesa;
 import br.com.alura.orcamentoapi.model.Despesa;
-import br.com.alura.orcamentoapi.model.FORM.RequestDespesa;
-import br.com.alura.orcamentoapi.model.FORM.ResponseUser;
-import br.com.alura.orcamentoapi.model.Usuario;
 import br.com.alura.orcamentoapi.model.ValorCategoria;
 import br.com.alura.orcamentoapi.repository.DespesaRepository;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,13 +16,13 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 @DisplayName("Testes de Despesa Repository")
+@Log4j2
 class DespesaRepositoryTest {
 
     @Autowired
@@ -34,47 +32,42 @@ class DespesaRepositoryTest {
     private TestEntityManager em;
 
     @Test
-    void get_RetornaListaDeDespesasPorDescricao_WhenSuccessful() {
-        String descricao = "conta";
+    @DisplayName("Find By Descricao returns list of Despesa when successful")
+    void findByDescricao_ReturnListOfDespesa_WhenSuccessful() {
         Despesa despesa = createDespesa();
-        despesa.setDescricao(descricao);
 
-        List<Despesa> despesas = repository.findByDescricao(descricao);
+        List<Despesa> despesas = repository.findByDescricao(despesa.getDescricao());
 
-        Assertions.assertTrue(despesas.stream().anyMatch(d -> d.getDescricao().equals(descricao)));
+        Assertions.assertTrue(despesas.contains(despesa));
     }
 
     @Test
-    void get_NaoRetornaListaDeDespesasPorDescricao_WhenFailed() {
-        String descricao = "inexistente";
+    @DisplayName("Find By Descricao returns empty when no Despesa is not found")
+    void findByDescricao_ReturnEmptyList_WhenDespesaIsNotFound() {
+        List<Despesa> despesas = repository.findByDescricao("descricao");
 
-        List<Despesa> despesas = repository.findByDescricao(descricao);
-
-        Assertions.assertTrue(despesas.stream().noneMatch(d -> d.getDescricao().equals(descricao)));
+        Assertions.assertTrue(despesas.isEmpty());
     }
 
     @Test
-    void get_RetornaListaDeDespesasPorDataEntreDataInicialEDataFinal_WhenSuccessful() {
+    @DisplayName("Find By Data Between returns list of Despesa between start date and end date when successful")
+    void findByDataBetween_ReturnListOfDespesa_WhenSuccessful() {
         LocalDate dataIni = LocalDate.of(2000,12,2);
         LocalDate dataFin = LocalDate.of(2000,12,30);
 
         Despesa despesa = createDespesa();
         despesa.setData(LocalDate.of(2000,12,15));
-        em.persist(despesa);
 
         List<Despesa> despesas = repository.findByDataBetween(dataIni,dataFin);
 
-        Assertions.assertTrue(despesas.stream().anyMatch(d -> d.getData().equals(despesa.getData())));
+        Assertions.assertTrue(despesas.contains(despesa));
     }
 
     @Test
-    void get_naoRetornaListaDeDespesasPorDataEntreDataInicialEDataFinal_WhenFailed() {
+    @DisplayName("Find By Data Between returns empty list when no Despesa is between star date and end date")
+    void findByDataBetween_ReturnEmptyList_WhenSuccessful() {
         LocalDate dataIni = LocalDate.of(2000,12,2);
         LocalDate dataFin = LocalDate.of(2000,12,30);
-
-        Despesa despesa = createDespesa();
-        despesa.setData(LocalDate.of(2000,12,1));
-        em.persist(despesa);
 
         List<Despesa> despesas = repository.findByDataBetween(dataIni,dataFin);
 
@@ -82,18 +75,18 @@ class DespesaRepositoryTest {
     }
 
     @Test
-    void get_RetornaValorTotalPorCategoriaECategoriasEntreDataInicialEDataFinal_WhenSuccessful() {
+    @DisplayName("Busca Valor Total Por Categoria returns list of ValorCategoria between start date and end date when successful")
+    void buscaValorTotalPorCategoria_ReturnListOfValorCategoria_WhenSuccessful() {
         LocalDate dataIni = LocalDate.of(2000,12,2);
         LocalDate dataFin = LocalDate.of(2000,12,30);
 
         Despesa despesa = createDespesa();
-        em.persist(despesa);
+        despesa.setData(LocalDate.of(2000, 12, 15));
 
         List<ValorCategoria> valorCategorias = repository.buscaValorTotalPorCategoria(dataIni,dataFin);
 
-        Assertions.assertTrue(valorCategorias.stream().allMatch(
-                c -> c.getCategoria().equals(despesa.getCategoria().getDescricao())
-                && c.getTotal().equals(despesa.getValor())));
+        Assertions.assertEquals(valorCategorias.get(0).getCategoria(), despesa.getCategoria().getDescricao());
+        Assertions.assertEquals(valorCategorias.get(0).getTotal(), despesa.getValor());
     }
 
     @Test
@@ -103,7 +96,6 @@ class DespesaRepositoryTest {
 
         Despesa despesa = createDespesa();
         despesa.setData(LocalDate.of(2000, 12, 1));
-        em.persist(despesa);
 
         List<ValorCategoria> valorCategorias = repository.buscaValorTotalPorCategoria(dataIni,dataFin);
 
